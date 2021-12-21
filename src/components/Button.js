81ba129c895e7dc73b3ede8fad5668285395f807
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-12-21 16:33:05 -0500

    react refresher

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+function Button(){
+    return <button className="button" style={{
+        backgroundColor: "blue",
+        padding: "10px",
+        color: "white"
+        }}>
+        Button
+        </button>
+}
+export default Button
